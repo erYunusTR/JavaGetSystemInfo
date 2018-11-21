079817b0dc7d2fd2f7566a146ca9ba96dae85a4e
yer <yer@hotech.systems>
2018-11-21 19:10:51 +0300

    init

--- src/ip/Main.java
+++ src/ip/Main.java
@@ -3,7 +3,9 @@ package ip;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.URL;
+import java.net.UnknownHostException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,25 +15,47 @@ public class Main {
 
         deneme2();
 
+        System.out.println(getComputername());
 
+        System.out.println(getIpAdress());
     }
 
     private static void deneme1() throws IOException {
-        URL whatismyip = new URL("http://checkip.amazonaws.com");
-        BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
+        String ip = null;
+        try {
+            URL whatismyip = new URL("http://checkip.amazonaws.com");
+            BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
+            ip = in.readLine();
+        } catch (IOException ignored) {
 
-        String ip = in.readLine();
-        System.out.println(ip);
+        }
+
+        System.out.println(ip);  //internet yoksa null dönüyo
     }
 
 
     private static void deneme2() throws IOException {
-        URL whatismyip = new URL("http://myexternalip.com/raw");
-        BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
+        String ip = null;
+        try {
+            URL whatismyip = new URL("http://myexternalip.com/raw");
+            BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
+            ip = in.readLine();
+        } catch (IOException ignored) {
 
-        String ip = in.readLine();
-        System.out.println(ip);
+        }
+
+        System.out.println(ip);   //internet yoksa null dönüyo
     }
 
+    public static String getComputername() throws UnknownHostException {
+
+        return InetAddress.getLocalHost().getHostName();
+    }
+
+
+    public static String getIpAdress() throws UnknownHostException {
+
+        return InetAddress.getLocalHost().getHostAddress();
+    }
 
 }
